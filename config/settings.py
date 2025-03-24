12d403479c293ceb3e89040a8dd4c2b17eccdcb3
Nova <bushdog094@gmail.com>
2025-03-24 15:06:53 +0900

    "

--- config/settings.py
+++ config/settings.py
@@ -198,11 +198,17 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+if DEBUG:
+    CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:3000"]
+    CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
+
+else:
+    CORS_ALLOWED_ORIGINS = ["https://airbnb-clone-frontend-x4pp.onrender.com"]
+    CSRF_TRUSTED_ORIGINS = ["https://airbnb-clone-frontend-x4pp.onrender.com"]
+
 
 CORS_ALLOW_CREDENTIALS = True
 
-CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1:3000"]
 
 GH_SECRET = env("GH_SECRET")
 
